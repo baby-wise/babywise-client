9c6d03ab73fed28c19aa4e27b802e0e72a370a34
Tomás D'Antonio <t.dantonio@outlook.com>
2025-08-03 21:28:41 -0300

    add old signaling server

--- BabyWiseAPI/src/index.js
+++ BabyWiseAPI/src/index.js
@@ -8,23 +8,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
-<<<<<<< HEAD
 import B2 from 'backblaze-b2';
+import { admin } from './config/firebaseConfig.js';
 import { router as bucketRoutes } from './routes/bucket.routes.js';
+import { router as userRoutes } from './routes/users.routes.js';
+import { router as groupRoutes } from './routes/group.routes.js';
 import { WebSocketServer } from 'ws';
 
 dotenv.config();
-=======
-import { Server } from 'socket.io';
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-dotenv.config()
-import B2 from 'backblaze-b2'
-import { admin } from './config/firebaseConfig.js';
-import {router as bucketRoutes} from './routes/bucket.routes.js'
-import { router as userRoutes } from './routes/users.routes.js';
-import { router as groupRoutes } from './routes/group.routes.js';
->>>>>>> main
 
 const app = express();
 app.use(cors());
@@ -41,7 +32,15 @@ const receiver = new WebhookReceiver(apiKey, apiSecret);
 const egressClient = new EgressClient(livekitEgressUrl, apiKey, apiSecret);
 const wsAudioPath = '/audio-egress';
 const wss = new WebSocketServer({ server: httpServer, path: wsAudioPath });
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+let clients = [];
 
+// Socket connection for TrackEgress data input
 wss.on('connection', (ws, req) => {
   console.log('[WS] Nueva conexión de egress para análisis de audio');
   let audioBuffers = [];
@@ -92,7 +91,6 @@ wss.on('connection', (ws, req) => {
     wav.encode(audioData).then(wavBuffer => {
       fs.writeFileSync(wavPath, Buffer.from(wavBuffer));
       console.log(`[WS] Archivo WAV guardado: ${wavPath}`);
-      // Aquí podrías llamar a tu IA con wavPath
     }).catch(err => {
       console.error('[WS] Error al guardar WAV:', err);
     });
@@ -125,8 +123,81 @@ wss.on('connection', (ws, req) => {
   });
 });
 
-<<<<<<< HEAD
-// Necesario para recibir el body crudo
+// Socket connection for mobile clients
+io.on('connection', (socket) => {
+    console.log(`Cliente conectado: ${socket.id}`);
+
+    // Evento para unirse a una sala (ej. la sala del bebé)
+    socket.on('join-room', (data) => {
+        socket.join(data.group);
+        console.log(`${data.role} ${socket.id} se unió al grupo: ${data.group}`);
+        const clientInfo = { socket, role: data.role, group: data.group };
+        clients.push(clientInfo);
+    });
+
+    socket.on('add-camera', (data) => {
+        const camerasId = clients
+            .filter((c) => c.role === 'camera' && c.group === data.group)
+            .map((c) => c.socket.id);
+        
+        const viewerSockets = clients
+            .filter(c => c.role === 'viewer' && c.group === data.group)
+            .map((c) => c.socket.id);
+        
+        viewerSockets.forEach(sId =>{
+            console.log(`Enviandole al ${sId} que hay una camara disponible`)
+            socket.to(sId).emit('cameras-list', camerasId)})
+    });
+
+    socket.on('get-cameras-list', (data) => {
+        const camerasId = clients
+            .filter((c) => c.role === 'camera' && c.group === data.group)
+            .map((c) => c.socket.id);
+        socket.emit('cameras-list', camerasId);
+    });
+
+    // Notificar a los otros en la sala que un nuevo par se ha unido
+    socket.on('start-stream', (data) => {
+            /*
+            Nota: Esto funciona igual a como lo teniamos antes pero se tiene que cambiar para que 
+            busque al cliente camara de ese grupo con ese socketId y mandarle solo a ese      
+            */
+        socket.to(data.group).emit('peer-joined', { peerId: socket.id });
+    });
+    
+    // Reenviar la oferta de WebRTC a los otros pares en la sala
+    socket.on('offer', (payload) => {
+        console.log(`Recibida oferta de ${socket.id}, reenviando a ${payload.targetPeerId}`);
+        io.to(payload.targetPeerId).emit('offer', {
+            sdp: payload.sdp,
+            sourcePeerId: socket.id,
+        });
+    });
+
+    // Reenviar la respuesta de WebRTC al par original
+    socket.on('answer', (payload) => {
+        console.log(`Recibida respuesta de ${socket.id}, reenviando a ${payload.targetPeerId}`);
+        io.to(payload.targetPeerId).emit('answer', {
+            sdp: payload.sdp,
+            sourcePeerId: socket.id,
+        });
+    });
+
+    // Reenviar los candidatos ICE
+    socket.on('ice-candidate', (payload) => {
+        io.to(payload.targetPeerId).emit('ice-candidate', {
+            candidate: payload.candidate,
+            sourcePeerId: socket.id,
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Cliente desconectado: ${socket.id}`);
+        clients = clients.filter(c => c.socket.id !== socket.id)
+    });
+});
+
+// Webhook for LiveKit events
 app.use('/webhook', express.raw({ type: 'application/webhook+json' }));
 app.post('/webhook', async (req, res) => {
   try {
@@ -156,13 +227,8 @@ app.post('/webhook', async (req, res) => {
     res.status(400).send('invalid webhook');
   }
 });
-=======
-// Conexión a MongoDB
-mongoose.connect(`mongodb+srv://babywise2025:${process.env.MONGO_PW}@babywise.aengkd2.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=${process.env.MONGO_APP_NAME}`)
-  .then(() => console.log("Conectado a MongoDB"))
-  .catch((error) => console.log("Error de conexión a MongoDB:", error));
->>>>>>> main
 
+// Token endpoint for LiveKit
 app.get('/getToken', async (req, res) => {
   const { roomName, participantName } = req.query;
   if (!roomName || !participantName) {
@@ -177,7 +243,6 @@ app.get('/getToken', async (req, res) => {
     identity: participantName,
   });
 
-<<<<<<< HEAD
   const videoGrant = {
     room: roomName,
     roomJoin: true,
@@ -185,12 +250,6 @@ app.get('/getToken', async (req, res) => {
     canSubscribe: true,
   };
   at.addGrant(videoGrant);
-=======
-//Rutas permitidas
-app.use(bucketRoutes)
-app.use(userRoutes)
-app.use(groupRoutes)
->>>>>>> main
 
   at.toJwt().then(token => {
     console.log('access token', token);
@@ -203,8 +262,11 @@ app.use(groupRoutes)
 
 (async () => {
   try {
-    // Connect to MongoDB
-    await mongoose.connect(`mongodb+srv://babywise2025:${process.env.MONGO_PW}@babywise.aengkd2.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.MONGO_APP_NAME}`);
+    // Conexión a MongoDB
+    mongoose.connect(`mongodb+srv://babywise2025:${process.env.MONGO_PW}@babywise.aengkd2.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&appName=${process.env.MONGO_APP_NAME}`)
+      .then(() => console.log("Conectado a MongoDB"))
+      .catch((error) => console.log("Error de conexión a MongoDB:", error));
+
     console.log("-> Connected to MongoDB");
 
     // Start the HTTP server
@@ -217,6 +279,8 @@ app.use(groupRoutes)
 })();
 
 app.use(bucketRoutes);
+app.use(userRoutes);
+app.use(groupRoutes);
 
 export const b2 = new B2({
   applicationKeyId: process.env.B2_KEY_ID,
