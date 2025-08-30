d8808efca12efe6fa4e4f3ee7d87f8c1e04d75ff
Tomás D'Antonio <t.dantonio@outlook.com>
2025-08-30 15:13:48 -0300

    refactor get recordings to bucket controller

--- BabyWiseAPI/src/routes/bucket.routes.js
+++ BabyWiseAPI/src/routes/bucket.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { upload, uploadAudio, listAudios, deleteAudio } from '../controllers/bucket.controller.js'
+import { upload, uploadAudio, listAudios, deleteAudio, handleGetRecordings } from '../controllers/bucket.controller.js'
 import multer from 'multer'
 const filesUploaded = multer({ dest: 'uploads/' }) // Lugar donde se guardan los archivos subidos
 
@@ -7,12 +7,7 @@ const filesUploaded = multer({ dest: 'uploads/' }) // Lugar donde se guardan los
 export const router = express.Router();
 
 router.post('/upload', filesUploaded.single('file'), upload)
-
-// Endpoint para subir audios personalizados
 router.post('/audios/upload', filesUploaded.single('file'), uploadAudio)
-
-// Endpoint para listar audios personalizados por room
 router.get('/audios', listAudios)
-
-// Endpoint para eliminar audios personalizados
 router.post('/audios/delete', deleteAudio)
+router.get('/recordings', handleGetRecordings);
--- BabyWiseAPI/src/routes/livekit.routes.js
+++ BabyWiseAPI/src/routes/livekit.routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { handleMediaServerEvent, handleTokenGrant, handleGetRecordings} from '../controllers/livekit.controller.js';
+import { handleMediaServerEvent, handleTokenGrant } from '../controllers/livekit.controller.js';
 
 const router = express.Router();
 
 router.use('/webhook', express.raw({ type: 'application/webhook+json' }));
 router.post('/webhook', handleMediaServerEvent);
 router.get('/getToken', handleTokenGrant);
-router.get('/recordings', handleGetRecordings);
 
 export { router };
